ffd918b854e0f8b70352b72fbe244936b1e97cf4
k3v1n <k3v1n0x90@gmail.com>
2023-12-09 23:35:19 +0100

    Solve day 2 part 2

--- Days/Day4/Day.cs
+++ Days/Day4/Day.cs
@@ -8,6 +8,9 @@ public static class Day
 
     var result1 = Solve1(input);
     Console.WriteLine(result1);
+
+    var result2 = Solve2(input);
+    Console.WriteLine(result2);
   }
 
   private static IEnumerable<string> ReadInput(string filename)
@@ -25,19 +28,62 @@ public static class Day
     return sum;
   }
 
-  private static (uint CardNumber, uint Points) ParseLine(ReadOnlySpan<char> line)
+  public static uint Solve2(IEnumerable<string> lines)
+  {
+    var listOfMatchingNumbersCounts = new List<uint>();
+
+    foreach (var line in lines)
+    {
+      var card = ParseLine(line);
+      listOfMatchingNumbersCounts.Add(card.CountOfMatchingNumbers);
+    }
+
+    return calculateNumberOfCards(listOfMatchingNumbersCounts);
+  }
+
+  private static uint calculateNumberOfCards(List<uint> listOfMatchingNumbersCounts)
+  {
+    uint totalNumberOfCards = 0u;
+    for (uint cardId = 1; cardId <= listOfMatchingNumbersCounts.Count; ++cardId)
+    {
+      totalNumberOfCards += calculateTotalCardCount(cardId, listOfMatchingNumbersCounts);
+    }
+
+    return totalNumberOfCards;
+  }
+
+  private static Dictionary<uint, uint> CARD_ID_TO_CARD_COUNT_CACHE = [];
+
+  private static uint calculateTotalCardCount(uint cardId, List<uint> listOfMatchingNumbersCounts)
+  {
+    if (CARD_ID_TO_CARD_COUNT_CACHE.TryGetValue(cardId, out var totalCardCount))
+    {
+      return totalCardCount;
+    }
+
+    uint totalCountOfCards = 1;
+    var numberOfExtraCards = cardId + listOfMatchingNumbersCounts[(int)(cardId - 1u)];
+    for (uint extraCard = cardId + 1u; extraCard <= numberOfExtraCards; ++extraCard)
+    {
+      totalCountOfCards += calculateTotalCardCount(extraCard, listOfMatchingNumbersCounts);
+    }
+
+    return CARD_ID_TO_CARD_COUNT_CACHE[cardId] = totalCountOfCards;
+  }
+
+  private static (uint CountOfMatchingNumbers, uint Points) ParseLine(ReadOnlySpan<char> line)
   {
     int i = 0;
-    int cardNumber = 0;
     var winningNumbers = new HashSet<int>();
+    var countOfMatchingNumbers = 0u;
     Span<char> numberDigits = stackalloc char[10];
 
-    for (; i < line.Length && !TryParseNextNumber(ref i, line, ref numberDigits, out cardNumber); ++i)
+    for (; i < line.Length && !TryParseNextNumber(ref i, line, in numberDigits, out _); ++i)
     { }
 
     for (; i < line.Length && line[i] != '|'; ++i)
     {
-      if (TryParseNextNumber(ref i, line, ref numberDigits, out int number))
+      if (TryParseNextNumber(ref i, line, in numberDigits, out int number))
       {
         winningNumbers.Add(number);
       }
@@ -46,18 +92,19 @@ public static class Day
     uint points = 0u;
     for (; i < line.Length; ++i)
     {
-      if (TryParseNextNumber(ref i, line, ref numberDigits, out int number) && winningNumbers.Contains(number))
+      if (TryParseNextNumber(ref i, line, in numberDigits, out int number) && winningNumbers.Contains(number))
       {
+        ++countOfMatchingNumbers;
         points = points == 0u
           ? 1u
           : points << 1;
       }
     }
 
-    return ((uint)cardNumber, points);
+    return (countOfMatchingNumbers, points);
   }
 
-  private static bool TryParseNextNumber(ref int index, ReadOnlySpan<char> line, ref Span<char> numberDigits, out int number)
+  private static bool TryParseNextNumber(ref int index, ReadOnlySpan<char> line, in Span<char> numberDigits, out int number)
   {
     int digitIndex = 0;
     while (index < line.Length && char.IsDigit(line[index]))
